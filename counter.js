a2d7d28aebaa7bb07a88273b307089579c3d9e4f
Ferid Mirzeyev <feridmrzyv2005@gmail.com>
2024-09-05 18:18:28 +0400

    counter

---
+++ counter.js
@@ -0,0 +1,17 @@
+const negativeBtn = document.getElementById("btn-negative")
+const positiveBtn = document.getElementById("btn-positive")
+const counter = document.getElementById("counter")
+const reset = document.getElementById("reset")
+
+let x=0;
+negativeBtn.addEventListener("click", function(){
+    x=x-1;
+    counter.innerHTML = x;
+});
+positiveBtn.addEventListener("click", function(){
+    x= x+1;
+    counter.innerHTML= x;
+});
+reset.addEventListener("click", function(){
+    counter.innerHTML = 0;
+});
